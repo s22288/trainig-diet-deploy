305091737fbb564543f234b6f27abea2b4337b94
„Michal <s22288@pjwstk.edu.pl>
2023-12-19 21:04:57 +0100

    close forbidden routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import AllRoutes from './pages/AllRoutes/AllRoutes';
+import { useAuthenticate } from './services/usersServices/UserService';
 
 function App() {
+  useAuthenticate()
   return (
     <div className="App">
       <AllRoutes />
--- src/components/Big/ownTrainings/smallTrainingDetails.jsx
+++ src/components/Big/ownTrainings/smallTrainingDetails.jsx
@@ -3,20 +3,36 @@ import FunctionalityNavbar from "../../Medium/navbar/functionalitynavbar";
 import { ReactComponent as Houselogo } from "../../../photo/house.svg";
 import { ReactComponent as TrainingLogo } from "../../../photo/training.svg";
 import { ReactComponent as NoteLogo } from "../../../photo/note.svg";
-
 import './smallTrainingDetails.css'
+import { useEffect, useState } from "react";
+import { checkUserRole } from "../../../services/usersServices/UserService";
 const SmallTrainigDetails = () => {
     const location = useLocation();
+    const [role, setRole] = useState('USER')
+
     const data = location.state?.data;
     console.log('dane' + data.day)
+    useEffect(() => {
+        checkUserRole().then((fulfilledValue) => {
+            const stringValue = String(fulfilledValue);
+            setRole(stringValue);
+        }, []);
 
 
+    }, [location.pathname])
     if (!data) {
         return <div>No exercise data available.</div>;
     }
+
+
     return (
         <div className="small-training-background" >
-            <FunctionalityNavbar />
+            {role === 'USER' ? (
+                <FunctionalityNavbar />
+            ) : (
+                <FunctionalityNavbar />
+            )}
+
             <div className="small-training-details-container" >
                 <h2><TrainingLogo /> {data.training.description} /{data.training.treiningType} </h2>
 
